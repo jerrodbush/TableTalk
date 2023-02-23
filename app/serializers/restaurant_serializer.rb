2506385f8999e941869a29105701a6be131d85b8
Jerrod <jerrodwilliambush@gmail.com>
2023-02-23 03:29:31 -0600

    business page + working maps

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,3 +1,3 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :phone, :address, :price, :open_time, :close_time, :capacity, :rest_image, :website
+  attributes :id, :name, :phone, :address, :price, :open_time, :close_time, :capacity, :rest_image, :website, :longitude, :latitude
 end
