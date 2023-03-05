85253d246539ba0af6691ea91c838585e195a092
jbush93 <106041074+jbush93@users.noreply.github.com>
2023-03-05 15:52:10 -0600

    Update restaurants_controller.rb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,12 +1,2 @@
 class RestaurantsController < ApplicationController
-    def index
-        restaurants = Restaurant.all
-        hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
-          marker.lat restaurant.latitude
-          marker.lng restaurant.longitude
-          marker.infowindow restaurant.name
-        end
-        render json: hash.to_json
-      end
-    end  
 end
