4b3ecad5eb301da679537dd0039dd270808c418c
John Rooney <rooneyjohn@gmail.com>
2023-03-06 15:50:38 -0500

    Added Index/Show for everything, plus rescues for Notfound/Invalid

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,17 @@
 class RatingsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :rating_not_found
+  def index
+    render json: Rating.all(), status: :ok
+  end
+
+  def show
+    myRating = Rating.find(params[:id])
+    render json: myRating, status: :ok
+  end
+
+  private
+
+  def rating_not_found
+    render json: {error: "Rating not found"}, status: :not_found
+  end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :res_not_found
   def index
     render json: Restaurant.all(), status: :ok
   end
@@ -8,5 +8,10 @@ class RestaurantsController < ApplicationController
     myRest = Restaurant.find(params[:id])
     render json: myRest, status: :ok
   end
-  
+
+  private
+
+  def res_not_found
+    render json: {error: "Restaurant not found"}, status: :not_found
+  end
 end
