05cc077868c5cb25f9703f6575c05074df09e6e9
John Rooney <rooneyjohn@gmail.com>
2023-03-06 12:01:10 -0500

    started working on Users controller/serailizer/model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,29 @@ class UsersController < ApplicationController
     myUser = User.find(params[:id])
     render json: myUser, serializer: ShowUserWithResSerializer, status: :ok
   end
+
+  def create
+    newUser = User.create!(strong_params)
+    render json: newUser, status: :created
+  end
+
+  def update
+    myUser = User.find(params[:id])
+    if myUser.update(strong_params)
+      render json: myUser, status: :ok
+    else
+      render json: { errors: myUser.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy!
+    head :no_content
+  end
+
+  private
+
+  def strong_params
+    params.permit(:first_name, :last_name, :age, :phone, :email, :location, :password, :user_image)
+  end
 end
