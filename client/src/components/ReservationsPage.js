c2ece2fabef1266ba40ff812947841eed3f50171
DylanRhinehart <dylanrhinehart@gmail.com>
2023-03-08 02:45:45 -0800

    added styling to reservations

--- client/src/components/ReservationsPage.js
+++ client/src/components/ReservationsPage.js
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar'
 import RenderReservations from './RenderReservations'
+import '../styling/reservation.css'
 
 export default function ReservationsPage() {
 
   return (
     <>
       <NavBar/>
-      <RenderReservations/>
+      <div className="reservations-container">
+        <div id="top-half-reserve">
+          <h2>Reservations</h2>
+          <select>
+            <option value="All">All</option>
+            <option value="rating">Rating</option>
+            <option value="price">Price</option>
+            <option value="I Pay">I Pay</option>
+            <option value="You Pay">You Pay</option>
+            <option value="We Pay">We Pay</option>
+          </select>
+        </div>
+      </div>
     </>
   )
 }
