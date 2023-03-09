81619d201f367b88e4c15386ba4c226b2cb10614
DylanRhinehart <dylanrhinehart@gmail.com>
2023-03-09 09:31:21 -0800

    reservations page

--- client/src/components/ReservationCard.js
+++ client/src/components/ReservationCard.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import '../App.css';
+import '../styling/reservationCard.css'
 
 export default function ReservationCard( { reservations }  ) {
   
@@ -25,11 +26,12 @@ export default function ReservationCard( { reservations }  ) {
   return (
     <div id="reservation-card-container">
         <div id="reservation-card-image-container">
-            <img src="http://placehold.it/250X250"/>
+            <img src={reservations.restaurant.rest_image}/>
             <div id="reservation-user-info-wrapper">
                 <h5 id="top-left">Dylan Rhinehart</h5>
+                <h6 id="top-left-check">{reservations.check_type}</h6>
                 <div id="top-right">
-                    <h4>{reservations.date}</h4>
+                    <h4 id="card-date">{reservations.date}</h4>
                     <h5>{reservations.time}:00PM</h5>
                 </div>
             </div>
