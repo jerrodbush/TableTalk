6756b6ca2c574ce8d2755ec4ff1b52627a57e824
Liza McLain <lizamclain@gmail.com>
2023-03-09 18:13:53 -0600

    added reservations_joined to users. added details for the reservation on members

--- app/serializers/member_serializer.rb
+++ app/serializers/member_serializer.rb
@@ -1,4 +1,4 @@
 class MemberSerializer < ActiveModel::Serializer
-  attributes :id, :reservation_id, :user_id, :guest_check_type
+  attributes :id, :reservation_id, :user_id, :guest_check_type, :details_for_reservation_as_member
 
 end
